bb40ca6690294101c907c203f11313523eef1912
owenkelbie <owenkelbie@labpc153.computing.dundee.ac.uk>
2019-01-15 12:51:59 +0000

    Commit Message

---
+++ src/helloworld/NewClass.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author owenkelbie
+ */
+public class NewClass {
+    
+    public static void Lenny(){
+        System.out.println("Lenny");
+    }
+    public static void David(){
+        System.out.println("David");
+    }
+}
