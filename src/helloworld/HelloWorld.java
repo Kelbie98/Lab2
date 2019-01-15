917cd6b7eb0e8c8a50dbfc8444c5c94a547dd9e7
owenkelbie <owenkelbie@labpc153.computing.dundee.ac.uk>
2019-01-15 12:45:40 +0000

    Commit Message

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author owenkelbie
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewClass Class = new NewClass();
+        for(int i=0;i<9;i++){ 
+            Class.Lenny();
+            if(i==4){
+                Class.David();
+            }
+        }
+    }
+
+ 
+}
